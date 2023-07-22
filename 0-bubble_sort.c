3ebba3d3af3b48c8e967167701eac0152f4714a2
cartermusee <cartermusee@gmail.com>
2023-07-22 11:50:03 -0700

    sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
+	if (array == NULL || !size)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1 - i ; j++)
