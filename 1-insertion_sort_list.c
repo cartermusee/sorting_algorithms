ff068524676403449fc566f228dd0b377b7cc825
cartermusee <cartermusee@gmail.com>
2023-07-24 11:06:25 -0700

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * swap - swaps 2 node
+ * @a: address of first node
+ * @b: address of second node
+ */
+void swap(listint_t *n, listint_t *k)
+{
+	if (n->prev)
+		n->prev->next = k;
+	if (k->next)
+		k->next->prev = n;
+	n->next = k->next;
+	k->prev = n->prev;
+	n->prev = k;
+	k->next = n;
+
+}
+
+/**
+ * insertion_sort_list - insertion sorts in doubly list
+ * @list: address of pointer to node head
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *a, *n;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	a = (*list)->next;
+	while (a)
+	{
+		n = a;
+		a = a->next;
+		while (n && n->prev)
+		{
+			if (n->prev->n > n->n)
+			{
+				swap(n->prev, n);
+				if (!n->prev)
+					*list = n;
+				print_list((const listint_t *)*list);
+			}
+			else
+				n = n->prev;
+		}
+
+	}
+}
